5522dd2096107d7b22e9c8e8f89b94a465c6e659
bradengl <bradengl@umich.edu>
2015-09-20 19:49:50 -0400

    Update list.c

--- list.c
+++ list.c
@@ -10,6 +10,27 @@
 list_t* insert_sorted(list_t* head, list_t* new_element) {
 	assert(head != NULL);
 	assert(new_element != NULL);
+	list_t* tempPointer = head;
+	int inserted = 0;
+	if( new_element->index < tempPointer->index ) {
+	  new_element->next = head;
+	  head = new_element;
+	  inserted = 1;
+	}
+
+	while( inserted==0 && tempPointer->next != NULL ) {
+	  if(new_element->index < tempPointer->next->index) {
+	    new_element->next = tempPointer->next;
+	    tempPointer->next = new_element;
+	    inserted = 1;
+	  }
+	  else {
+	    tempPointer = tempPointer->next;
+	  }
+	}
+	if( inserted==0 ) {
+	  tempPointer->next = new_element;
+	}
 
 	return head;
 }
@@ -19,6 +40,29 @@ list_t* insert_sorted(list_t* head, list_t* new_element) {
 list_t* reverse(list_t* head) {
 	assert(head != NULL);
 
+	list_t* tempPointer = head;
+	list_t* tempNext = head->next;
+	list_t* tempPrev = NULL;
+	tempPointer->next = tempPrev;
+	tempPrev = tempPointer;
+	tempPointer=tempNext;
+	
+	while( tempPointer->next != NULL ) {
+	  tempNext = tempPointer->next;
+	  tempPointer->next = tempPrev;
+	  tempPrev = tempPointer;
+	  tempPointer=tempNext;
+	}
+
+	tempNext = tempPointer->next;
+        tempPointer->next = tempPrev;
+	tempPrev = tempPointer;
+        
+	head = tempPointer;
+
+
 	return head;
 }
+
+
 
